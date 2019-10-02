00910b83dcc070d89e94d36e8da162419b4ad083
Extrogenesis <extrogenesis@gmail.com>
2019-10-02 11:29:10 -0400

    removed header and footer components; added work page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import Work from './pages/work/work'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      
+      <Work />
+     
     </div>
   );
 }
