fe425cec88a7e791edd5fb6fbd42650d3cd8f591
Extrogenesis <extrogenesis@gmail.com>
2019-10-01 22:13:43 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
