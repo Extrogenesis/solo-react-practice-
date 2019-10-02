fd41c29d0e8cf9592f9f5986086dd2c75e93f838
Extrogenesis <extrogenesis@gmail.com>
2019-10-02 09:57:46 -0400

    init commit

---
+++ src/pages/work/work.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './work.css'
+
+import Header from '../../components/Header/Header'
+import Footer from "../../components/Footer/Footer"
+
+function Work(){
+    return (
+        <div className="Work">
+            
+        </div>
+    )
+}
+
+export default Work
