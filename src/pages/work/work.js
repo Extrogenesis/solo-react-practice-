a6b0ae794792a783fa5dbf94a211ca43d5a601d5
Extrogenesis <extrogenesis@gmail.com>
2019-10-02 16:27:31 -0400

    reboot

--- src/pages/work/work.js
+++ src/pages/work/work.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons'
 
-
-
 import Header from '../../components/Header/Header'
 import Footer from "../../components/Footer/Footer"
 import './work.css'
@@ -13,6 +11,7 @@ import NJ from './img/night-journy-min.jpg'
 import DJ from './img/dan-johnson-min.jpg'
 import BN from './img/barney-newman-min.jpg'
 import BB from './img/Big bird-min.jpg'
+
 
 function Work(){
     return (
